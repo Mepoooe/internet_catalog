1e38fb2c9191ebfced5bb1ee6d4b6c255e7e6b68
777Vasya77 <777Vasya77>
2016-06-30 00:09:22 +0300

    added some frontend in the admin panel

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -75,10 +75,18 @@
             </div>
         </div>
     </nav>
+    <main class="main">
+        <div class="container">
+            <div class="row">
 
-    @yield('content')
+                @yield('content')
+                @yield('sidebar')
 
-        @yield('sidebar')
+
+            </div>
+        </div>
+    </main>
+    <footer class="footer"></footer>
 
     <!-- JavaScripts -->
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.2.3/jquery.min.js" integrity="sha384-I6F5OKECLVtK/BL+8iSLDEHowSAfUo76ZL9+kGAgTRdiByINKJaqTPH/QVNS1VDb" crossorigin="anonymous"></script>
