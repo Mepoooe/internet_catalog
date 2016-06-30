98aa4eed6d0840a3d7c8666acab78b9d88fbc64f
Misha Nikula <cj27111992@gmail.com>
2016-06-30 09:58:46 +0300

    form css

--- resources/views/admin/tables/createDrinks.blade.php
+++ resources/views/admin/tables/createDrinks.blade.php
@@ -6,18 +6,41 @@
         <li>{{$error}}</li>
         @endforeach
     @endif
-    
-    <form action="/admin/drinks" method="post" enctype="multipart/form-data" class="form-inline" >
-        <label for="">Название бутылки  <br><input type="text" class="input-medium" placeholder="Введите название" name="name""></label> <br>
-        <label for="">Цена за единицу <br> <input type="text" name="price"></label> <br>
-        <label for="">Объем <br> <input type="text" name="volume" ></label> <br>
-        <label for="">Тип напитка <br> 
-        <label for="">Алкогольный<input type="radio" name="typeDrink" value="alco"></label>
-        <label>Безалкогольный<input type="radio" name="typeDrink" value="soft"></label>
-
-        </label> <br>
-        <label for="">Катринка <br><input type="file" name="image"></label> 
-        <button>Отправить</button>
-        {{csrf_field()}}
-    </form>
+    <div class="row">
+    <div class="col-md-6 col-md-offset-3">
+        <form action="/admin/drinks" method="post" enctype="multipart/form-data">
+        <div class="col-sm-10">
+            <label class="col-sm-4 control-label" for="exampleInputName1">Название бутылки</label>
+            <input id="exampleInputName1" class="form-control" type="text" name="name">
+        </div>
+        <div class="col-sm-10">
+            <label class="col-sm-4 control-label" for="exampleInputName2">Цена за единицу</label> <input class="form-control" id="exampleInputName2" type="text" name="price"></label>
+        </div>
+        <div class="col-sm-10">
+            <label class="col-sm-4 control-label" for="exampleInputName3">Объем</label> 
+            <input id="exampleInputName3" class="form-control" type="text" name="volume">
+        </div>
+        <div class="col-sm-10">
+            <label class="col-sm-4 control-label" >Тип напитка:</label> 
+            <div class="radio">
+            
+            <label class="control-label">
+                    <input type="radio" name="typeDrink" value="alco"> Алкогольный 
+            </label>
+            <label class="control-label">
+                <input type="radio" name="typeDrink" value="soft"> Безалкогольный
+                </div>
+            </label> <br>
+            </div>
+            <div class="col-sm-10">
+                <label id="exampleInputFile" class="col-sm-4 control-label">Катринка</label>
+                <input type="file" id="exampleInputFile" name="image"></label> 
+            </div>
+            <div class="col-sm-10">
+                <button  class="btn btn-default">Отправить</button>
+            </div>
+            {{csrf_field()}}
+        </form>
+    </div>
+</div>
 @endsection
